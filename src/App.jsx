386b2c57bca10bea4cb8399b628c4208a4d58796
shonmathew19 <shonformal@gmail.com>
2024-09-25 17:25:45 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 import './App.css'
 import Header from './components/Header'
-import QuoteGenerator from './components/quoteGenerator'
+import QuoteGenerators from './components/QuoteGenerators';
 
 function App() {
 
   return (
     <>
       <Header />
-      <QuoteGenerator />
+      <QuoteGenerators />
     </>
   )
 }
